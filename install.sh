792a9e796470fe78e914486c166494326cb103fd
nwcfollowthewhiterabbit <100003631+nwcfollowthewhiterabbit@users.noreply.github.com>
2023-08-29 11:16:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-sudo apt update && apt upgrade -y && sudo apt install nodejs nginx npm -y
-sudo mkdir /web.app
-sudo git clone https://github.com/nwcfollowthewhiterabbit/test.git /web.app/
-cd /web.app/
-sudo sh install-back.sh
-sudo sh install-front.sh
+sudo apt update && apt dist-upgrade -y
+sudo apt upgrade -y & sudo apt install nodejs nginx npm certbot python3-certbot vite
+sudo git clone https://github.com/nwcfollowthewhiterabbit/test.git 
+cd test
+sudo mkdir /var/www/web.app && sudo chmod -R 644 /var/www/web.app
+sudo cp nginx.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+#sudo cp front.conf /etc/nginx/sites-available/ && sudo cp back.conf /etc/nginx/sites-available/
+#ln -s /etc/nginx/sites-available/front.conf /etc/nginx/sites-enabled/ && ln -s /etc/nginx/sites-available/back.conf /etc/nginx/sites-enabled/
+
+
+sh front-setup.sh
+sh back-setup.sh
+systemctl restart nginx
+#cp -r build/* /var/www/web.app/
